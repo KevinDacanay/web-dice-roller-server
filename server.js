a0b28270d83e49e77cab46890ce9e2c126d6295b
Kevin <kevin.dacanay27@gmail.com>
2026-02-28 19:54:13 -0600

    Updated server.js - minor change

--- server.js
+++ server.js
@@ -4,13 +4,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/static'))
-// app.use(cors({ origin: '*' }))
-
-// Route for the root to serve the API test page
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.use(express.static('public'));
 
 // 1. Wake Up API
 // Used to wake up the server asynchronously
